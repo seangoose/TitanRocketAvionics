43f520aa56eb748cee665579818b774e85512225
seangoose <sean.r.goose@gmail.com>
2026-02-01 22:28:38 -0800

    Refactor stage definition and fix typos

--- Teensy.ino
+++ Teensy.ino
@@ -11,7 +11,7 @@
 #include <Adafruit_BMP3XX.h>
 #include <Adafruit_LSM6DSOX.h>
 #include <Adafruit_ADXL375.h>
-const unsigned int Stage = 1; //Define stage up here, there will be stage dependant code later
+#define Stage 1 //Define stage up here, there will be stage dependant code later
 
 //Sensor Set up
 Adafruit_BMP3XX bmp;
@@ -35,7 +35,11 @@ struct FlightData {
 #define LoRaCS 37
 #define LoRaRST 9
 #define LoRaINT 8
-#define RF95_FREQ 915.0
+#define RF95_FREQ 915.0 // EDIT THIS TO CHANGE FREQUENCY
+// Bandwidth (seen below) Determines the head room of frequency we use, so with 125kHz, 915 would actually be 914.9375 - 915.0625
+// You should be looking into getting a HAM by now, you should use that brain of yours to figure something out about bandwidth, signal drift, etc.
+// Don't interfere with your own telemetry gang if that wans't obvious enough. Go at least 500 kHz different
+// For FCC compliance stay in the USA ISM Band, 902-928 MHz, and don't conflict with GPS freq.
 RH_RF95 rf95(LoRaCS, LoRaINT);
 
 //DFR0515 Specific Pins
@@ -45,12 +49,37 @@ RH_RF95 rf95(LoRaCS, LoRaINT);
 #define Gate1 2
 #define Gate2 3
 
+#define GATE1_DURATION 1000
+#define GATE2_DURATION 1000
+#define WATER_DURATION 3000
+
+#if Stage == 1
+  #define GATE1_FIRE_TIME 1000
+#elif Stage == 2
+  #define GATE1_FIRE_TIME 3000
+  #define GATE2_FIRE_TIME 1000
+#endif
+
+// For sensor change tracking
+
+float prev_altitude = 0;
+float altitude_velocity = 0;
+float prev_accel_mag = 0;
+
+// for stage seperation
+
+bool separation_detected = false;
+unsigned long separation_time = 0;
+
+// for apogee 
+bool apogee_detected = false; 
+
 // This is creating an instance of the flight data packet for later use
 FlightData currentPacket;
 
 //Logic for time keeping
 unsigned long lastTelemTime = 0;
-const long Tele_Rate = 20; // Runs loop every 20 ms which is (50 Hz)
+const long Tele_Rate = 100; // Runs loop every 100 ms which is (10 Hz)
 float BaseAGLPressure = 0;
 float sum = 0;
 
@@ -75,7 +104,7 @@ void setup() {
     while(1);
   }
   if (!rf95.setFrequency(RF95_FREQ)) {
-  Serail.println("LoRa frequency set failed");
+  Serial.println("LoRa frequency set failed");
   while(1);
 
   }
@@ -83,17 +112,18 @@ void setup() {
   rf95.setTxPower(23, false);
   //false has to do with the PA_BOOST pin whatever that is
   rf95.setSpreadFactor(8);
-  ref95.setSignalBandwidth(125E3);
-  ref95.setCodingRate4(5);
+  rf95.setSignalBandwidth(125E3);
+  rf95.setCodingRate4(5);
 
   //Set up Sensors
   if (!bmp.begin_I2C()) {
-    serial.print("BMP Error");
+    Serial.print("BMP Error");
     while(1);
   } 
   //BMP config, stuff straight out of the BMP3 Bosch code
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_2X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
   for(int i=0; i<20; i++) { //logic for getting the pressure on the rail
@@ -107,22 +137,54 @@ void setup() {
   Serial.println(BaseAGLPressure);
 
   if (!lsm6ds.begin_I2C()) {
-    serial.print("LSM Error");
+    Serial.print("LSM Error");
     while(1);
   }
+
+  // if (currentPacket.Stage == 1) {
+
+  // enum RocketState {
+  //   PAD          = 0,  // On ground, armed
+  //   BOOST        = 1,  // Motor firing (accel > 3g)
+  //   COAST        = 2,  // Post-burnout, ascending (accel < 1g)
+  //   SEPARATION   = 3,  // Separation event detected
+  //   DESCENT      = 4,  // Descending (altitude decreasing)
+  //   LANDED       = 5   // On ground (low altitude + no movement)
+  // };
+
+  // }
+
+  // if (currentPacket.Stage == 2) { 
+
+  // enum RocketState {
+  //   PAD           = 0,  // On ground, armed
+  //   BOOST_S1      = 1,  // Stage 1 boosting (passenger)
+  //   COAST_S1      = 2,  // Stage 1 coasting (passenger)
+  //   SEPARATION    = 3,  // Separation detected
+  //   COAST_S2      = 4,  // Post-separation coast
+  //   BOOST_S2      = 5,  // Sustainer firing
+  //   COAST_APOGEE  = 6,  // Coasting to apogee
+  //   APOGEE        = 7,  // At apogee, deploy water
+  //   DESCENT       = 8,  // Descending
+  //   LANDED        = 9   // On ground
+  // };
+
+  // }
+
 
 //LSM6DOX config
   lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
   lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_2000_DPS);
-  lsm6ds.setAccelDataRate(LSM6DS_Rate_104_HZ);
+  lsm6ds.setAccelDataRate(LSM6DS_RATE_104_HZ);
   lsm6ds.setGyroDataRate(LSM6DS_RATE_104_HZ);
 
   if (!adxl.begin()) {
-    serial.print("ADXL Error");
+    Serial.print("ADXL Error");
     while(1);
   }  
   adxl.setDataRate(ADXL343_DATARATE_100_HZ);
   currentPacket.state = 0; // setting equal to for On the ground 
+  currentPacket.Stage = Stage;
 }
 
 //Start main loop by reading data
@@ -144,7 +206,7 @@ void loop() {
   sensors_event_t event; //Weird ADAfruit code
   adxl.getEvent(&event);
   currentPacket.accelThrust = event.acceleration.x; //Becuase of how its mounted this is our vertical accel
-  currentPacket.accelZLateral = event.accleration.z; // Perpendicular accel
+  currentPacket.accelZLateral = event.acceleration.z; // Perpendicular accel
 
   //LSM Data
   sensors_event_t lsm_accel, lsm_gyro, lsm_temp;
@@ -156,9 +218,8 @@ void loop() {
   currentPacket.gyroYaw = lsm_gyro.gyro.z;
 
   //Finally send the packet of flight data
-  rf95.send((unit_8t *)&currentPacket, sizeof(currentPacket));
+  rf95.send((unit8_t *)&currentPacket, sizeof(currentPacket));
   rf95.waitPacketSent();
-
 
   //Debug of flgiht data
   Serial.print("+T"); Serial.print(currentPacket.timestamp);
@@ -167,4 +228,5 @@ void loop() {
   Serial.print(" | Spin: "); Serial.println(currentPacket.gyroRoll);
 
   }
+
 }
